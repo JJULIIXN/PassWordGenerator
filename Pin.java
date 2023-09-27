b8760bb773aa2245d6ae36ad0653d782378671e8
JJULIIXN <juliancruzet@gmail.com>
2023-09-27 15:44:51 -0400

    new update
    work

--- Pin.java
+++ Pin.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;  // Import the Scanner class
 
-public class Pin extends Password{
-    super(pinLen,password);
-    int counter;
+public class Pin extends Password {
+    int pinLen, counter;
+    String password;
+
+    public Pin(int pinLen){
+        super(
+        this.pinLen = pinLen;
+    }
+
+
 
     if(counter < pinLen){
         int i = (int)Math.floor(Math.random() * (0 - 9 + 1) + 0);
